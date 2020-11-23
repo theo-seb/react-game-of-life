d26541d6502226c6d3a6c7d5361c7d5c6ddcaddd
Theo SEBASTIEN <theo.sebast@gmail.com>
2020-11-23 18:56:09 +0100

    add a header bar with a start button, click to be implemented

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import {useState} from 'react';
+import { useState } from 'react';
 
 const COLUMNS = 20;
 const ROWS = 10;
+const HEADER_HEIGHT_PX = 60;
 
 function App() {
   const emptyGrid = Array(ROWS).fill().map((x) => Array(COLUMNS).fill(0));
@@ -14,28 +15,50 @@ function App() {
   };
 
   return (
-    <div>
-      {
-        grid.map((row, i) => (
-          <div key={i} style={{
-            display: 'flex',
-            flexDirection: 'row',
-          }}>{
-            row.map((cell, j) => (
-              <div 
-                key={`${i}-${j}`} 
-                style={{
-                  backgroundColor: cell ? 'black': 'white',
-                  border: '1px black solid',
-                  height: `${100 / ROWS}vh`,
-                  width: `${100 / COLUMNS}vw`,
-                }}
-                onClick={() => onCellClick(i, j)} />
-            ))
-          }</div>)
-        )
-      }
-    </div>
+    <>
+      <header style={{
+        height: `${HEADER_HEIGHT_PX}px`, 
+        backgroundColor: 'black', 
+        width: '100vw',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'flex-end',
+        alignItems: 'center'
+      }}>
+        <button style={{
+          margin: '10px',
+          padding: '10px 20px',
+          backgroundColor: 'lightgrey',
+          textAlign: 'center',
+          border: 'none',
+          borderRadius: '5px',
+          fontWeight: 'bold',
+          fontSize: '18px',
+        }}>Start</button>
+      </header>
+      <div>
+        {
+          grid.map((row, i) => (
+            <div key={i} style={{
+              display: 'flex',
+              flexDirection: 'row',
+            }}>{
+                row.map((cell, j) => (
+                  <div
+                    key={`${i}-${j}`}
+                    style={{
+                      backgroundColor: cell ? 'black' : 'white',
+                      border: '1px black solid',
+                      height: `calc(${100 / ROWS}vh - ${HEADER_HEIGHT_PX / ROWS}px)`,
+                      width: `${100 / COLUMNS}vw`,
+                    }}
+                    onClick={() => onCellClick(i, j)} />
+                ))
+              }</div>)
+          )
+        }
+      </div>
+    </>
   );
 }
 
