871ee091d1f8643602f1c999952f3e23128c0e85
Theo SEBASTIEN <theo.sebast@gmail.com>
2020-11-23 16:22:07 +0100

    draw basic grid based on number of columns & rows

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import {useState} from 'react';
+
+const COLUMNS = 20;
+const ROWS = 10;
+
 function App() {
+  const emptyGrid = Array(ROWS).fill().map((x) => Array(COLUMNS).fill(0));
+  const [grid, setGrid] = useState(emptyGrid);
+
   return (
     <div>
-      TODO
+      {
+        grid.map((row, i) => (
+          <div key={i} style={{
+            display: 'flex',
+            flexDirection: 'row',
+          }}>{
+            row.map((cell, j) => (
+              <div key={`${i}-${j}`} style={{
+                backgroundColor: cell ? 'black': 'white',
+                border: '1px black solid',
+                height: '10px',
+                width: '10px',
+              }} />
+            ))
+          }</div>)
+        )
+      }
     </div>
   );
 }
