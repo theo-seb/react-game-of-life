e0b663517c1dc76d190f3bbd2222b3923909675b
Theo SEBASTIEN <theo.sebast@gmail.com>
2020-11-26 06:22:02 +0100

    change indentation for current components

--- src/components/SizeInput.jsx
+++ src/components/SizeInput.jsx
@@ -1,17 +1,17 @@
-const SizeInput = ({value, onChange}) => {
-    return (
-        <input type="number" value={value}
-            onChange={onChange} style={{
-                margin: '10px',
-                padding: '10px 0',
-                border: 'none',
-                borderRadius: '5px',
-                fontWeight: 'bold',
-                fontSize: '18px',
-                width: '50px',
-                textAlign: 'center',
-            }} />
-    );
+const SizeInput = ({ value, onChange }) => {
+  return (
+    <input type="number" value={value}
+      onChange={onChange} style={{
+        margin: '10px',
+        padding: '10px 0',
+        border: 'none',
+        borderRadius: '5px',
+        fontWeight: 'bold',
+        fontSize: '18px',
+        width: '50px',
+        textAlign: 'center',
+      }} />
+  );
 };
 
 export default SizeInput;
