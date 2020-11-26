6e2c8a0c4b41485284b2d2cbe7cc7bb212c8c27c
Theo SEBASTIEN <theo.sebast@gmail.com>
2020-11-26 18:58:12 +0100

    remove key prop from cell component

--- src/components/Cell.jsx
+++ src/components/Cell.jsx
@@ -1,11 +1,10 @@
 const Cell = ({
-  key,
   onCellClick,
   color,
   size: { height, width }
 }) => {
   return (
-    <div key={key} onClick={onCellClick} style={{
+    <div onClick={onCellClick} style={{
       backgroundColor: color,
       border: '1px black solid',
       height,
